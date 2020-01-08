907a26d7f9f8662dd1966556202d858293726d4b
Vikram Kumar <vickykumarmlt@gmail.com>
2020-01-08 16:04:13 +0530

    changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from maven sample project ssdaddafsfshal" );
+        System.out.println( "Hello World from maven sample project and this output tested with jenkins" );
     }
 }
